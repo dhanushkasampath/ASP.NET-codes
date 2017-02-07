bdb00583ffe9fe5a5c19f4e680e1ec26d3ae7c46
H.K.D.Sampath Kumara <sampath.kumara@aiesec.net>
2017-02-07 10:07:45 +0530

    Add files via upload

---
+++ database CRUD/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ViewData.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+            <td>Gender:</td>
+            <td><asp:TextBox ID="txtGender" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+            <td>Department:</td>
+            <td><asp:TextBox ID="txtDepartment" runat="server"></asp:TextBox></td>
+        </tr>
+    </table>
+    
+    </div>
+        
+        <p>
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Update" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="LoadData" />
+            
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+            
+        </p>
+        <asp:HiddenField ID="HiddenField1" runat="server" />
+        
+    </form>
+</body>
+</html>
---
+++ database CRUD/WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ViewData
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadEmployee();
+            }
+        }
+        protected void LoadEmployee()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))//make the connetion object using connection string
+            {
+                string sqlQuery = "select id,name,gender,depname from tblemployee where id=205";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                con.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        txtName.Text = rdr["Name"].ToString();
+                        txtGender.Text = rdr["Gender"].ToString();
+                        txtDepartment.Text = rdr["DepName"].ToString();
+                        HiddenField1.Value = rdr["Id"].ToString();
+                    }
+                }
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {//query ya liwwa
+                String sqlQuery = "update tblemployee set name=@name,gender=@gender,depname=@depname where id=@id";
+                // dan eka sqlcommand ekata pass karanna ona
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@gender", txtGender.Text);
+                cmd.Parameters.AddWithValue("@depname", txtDepartment.Text);
+                cmd.Parameters.AddWithValue("@id", HiddenField1.Value);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                txtName.Text = "";
+                txtGender.Text = "";
+                txtDepartment.Text = "";
+
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            LoadEmployee();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            String CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {//query ya liwwa
+                String sqlQuery = "delete name,gender,depname from tblemployee where id=@id";
+                // dan eka sqlcommand ekata pass karanna ona
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                txtName.Text = "";
+                txtGender.Text = "";
+                txtDepartment.Text = "";
+
+
+            }
+        }
+    }
+}
